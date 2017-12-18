8af5e1b149c81651d541df04c185485a635b049a
neramas1221 <connorwheeler1997@gmail.com>
2017-12-18 19:42:18 +0000

    updated calculate network function
    function now uses the correct maths and give the right output back out

--- calculateNetwork.m
+++ calculateNetwork.m
@@ -6,12 +6,14 @@ weightSum =0;
 actiationSum =0;
 
 for i=1:X
-    activation = exp(-1/(2*sigma^2)* ((input(1,1) - nodes(i,1)^2) ...
-       + (input(2,1) - nodes(i,2)^2)+ (input(3,1) - nodes(i,3)^2)));
+    diff = ((input(1,1) - nodes(i,1))^2 ...
+       + (input(2,1) - nodes(i,2))^2 + (input(3,1) - nodes(i,3))^2);
+    activation = exp(-1/(2*sigma^2)*diff );
     nodes(i,4) = activation;
     weightSum = weightSum + nodes(i,5)*activation;
     actiationSum = actiationSum + activation;
 end
+
 if weightSum ~= 0 && actiationSum ~=0
     networkOutput = weightSum/actiationSum; 
 else
