ae8b5ae2db926e7833c523674f497da886db11da
neramas1221 <connorwheeler1997@gmail.com>
2017-12-19 17:58:50 +0000

    Large commit
    lots of things got changed

--- main.m
+++ main.m
@@ -1,23 +1,40 @@
-%testingData = importdata('',',');
-trainingData = importdata('dataset.csv',',');
-sigma = 0.3;
+testingData = importdata('data\2016_hourly.csv',',');
+trainingData = importdata('data\2012_hourly.csv',',');
+
 [X,Y] = size(trainingData);
-nodes = zeros(X,5);
+[testX, testY] = size(testingData);
+num = floor(X/4);
+[id,centers] = kmeans(trainingData(:,1:end-1),num);
+
+[centerX, centerY] = size(centers);
+
+sigma = 0.2;
+
+%nodes = zeros(X,5);
+
+nodes = zeros(centerX,5);
+
 inputs = zeros(3,1);
-center = zeros(3,1);
-for i =1:X
-    nodes(i,1) =9999;
-    nodes(i,2) = 9999;
-    nodes(i,3) = 9999;
+
+%center = zeros(3,1);
+
+
+for i =1:centerX
+    nodes(i,1) = centers(i,1);
+    nodes(i,2) = centers(i,2);
+    nodes(i,3) = centers(i,3);
     nodes(i,4) = 0;
-    nodes(i,5) = 1;
+    nodes(i,5) = rand(1);
 end
-loopCount =5;
+
+loopCount = 20;
+
 netOutputArray = zeros(X,1);
+netOutputTest = zeros(testX,1);
 nodeRecruted =0;
 
 error = zeros(loopCount,1);
-
+errorTest = zeros(loopCount,1);
 for epoc=1:loopCount
     
     for i=1:X
@@ -31,35 +48,49 @@ for epoc=1:loopCount
         
         netOutputArray(i,1) = netoutPut;
         
-        for j =1:X
-            center(1,1) = nodes(j,1);
-            center(2,1) = nodes(j,2);
-            center(3,1) = nodes(j,3);
-            if center == inputs
+%         for j =1:centerX %X
+%             center(1,1) = nodes(j,1);
+%             center(2,1) = nodes(j,2);
+%             center(3,1) = nodes(j,3);
+            %if center == inputs
                 nodes = updateWeights(netoutPut,nodes,target);
-                nodeRecruted = 1;
-            end
-        end
+               % nodeRecruted = 1;
+            %end
+        %end
         
-        if nodeRecruted == 0
-            for k=1:X
-                if nodes(k,1) == 9999
-                    nodes(k,1) = inputs(1,1);
-                    nodes(k,2) = inputs(2,1);
-                    nodes(k,3) = inputs(3,1);
-                    nodes(k,5) = target;
-                    break;
-                    % k-means nearest neighbers
-                end
-            end
-        end
+%         if nodeRecruted == 0
+%             for k=1:X
+%                 if nodes(k,1) == 9999
+%                     nodes(k,1) = inputs(1,1);
+%                     nodes(k,2) = inputs(2,1);
+%                     nodes(k,3) = inputs(3,1);
+%                     nodes(k,5) = target;
+%                     break;
+%                     % k-means nearest neighbers
+%                 end
+%             end
+%         end
     end
     error(epoc,1) = errorCalculation(trainingData,netOutputArray);
     fprintf("epoc number : %d complete\n",epoc);
+    
+    for i=1:testX
+        inputs(1,1) = testingData(i,1);
+        inputs(2,1) = testingData(i,2);
+        inputs(3,1) = testingData(i,3);
+        target = testingData(i,4);
+        
+        [netoutPut,nodes] = calculateNetwork(nodes,inputs,sigma);
+        
+        netOutputTest(i,1) = netoutPut;
+    end
+    errorTest(epoc,1) = errorCalculation(testingData,netOutputTest);
 end
+
 figure(1)
 hold on 
 plot(error,'b-');
+plot(errorTest,'r-');
 hold off
 
 figure(2)
--- updateWeights.m
+++ updateWeights.m
@@ -3,7 +3,11 @@ function [nodes] = updateWeights(netoutput,nodes,target)
 [X,Y] =size(nodes);
 learningRate =0.2;
 
-nodes(:,5) = nodes(:,5) + learningRate*(target-netoutput)*nodes(:,4);
+
+for i=1:X
+    nodes(i,5) = nodes(i,5) + learningRate*(target-netoutput)*nodes(i,4);
+end
+
 
 
 end
